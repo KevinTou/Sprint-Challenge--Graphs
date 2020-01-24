3416b9f0c095898f891568feb193fd1473a3f2d2
Kevin Tou <toukevin@gmail.com>
2020-01-23 18:48:11 -0800

    Added notes to help brainstorm

--- adv.py
+++ adv.py
@@ -8,16 +8,15 @@ from ast import literal_eval
 # Load world
 world = World()
 
-
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+map_file = "maps/test_loop_fork.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -29,6 +28,31 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+'''
+Notes:
+
+Example room graph:
+room_graph = {
+    0: [(3, 5), {'n': 1}], 
+    1: [(3, 6), {'s': 0, 'n': 2}], 
+    2: [(3, 7), {'s': 1}]
+}
+
+room_graph[room_id] = list[Tuple (3, 5), dict w/ possible exits {'n': 1}] # reference to the first room
+room_graph[0] = [(3, 5), {'n': 1}] # reference to the first room
+room_graph[0][0] = (3, 5) # x,y coords?
+room_graph[0][1] = {'n': 1} # list of possible exits
+
+Useful commands: 
+# Returns the current room's id ('int')
+    - player.current_room.id
+    # 0
+# Returns the current room's exits ('list')
+    - player.current_room.get_exits()
+    # ['n', 's', 'w', 'e']
+# Moves the player in the direction passed (direction = 'str')
+    - player.travel(direction)
+'''
 
 
 # TRAVERSAL TEST
@@ -41,22 +65,22 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
